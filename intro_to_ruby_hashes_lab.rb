8b0058f66edc0b355ec665725c5273a29152f9c9
Catherine Smiley <catherinesmiley@gmail.com>
2019-11-30 22:05:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,8 +22,10 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {
-	  :railroads => {:pieces => 4, :rent_in_dollars => {
-	    "1" => 5.04, "2" => 5.05, "3" => 5.06, "4"=> 5.07} 
+	  :railroads => {
+	    :pieces => 4, :rent_in_dollars => {
+	    "1" => 5.04, "2" => 5.05, "3" => 5.06, "4"=> 5.07},
+	    :names => "name"
 	  }
 	}
 end
