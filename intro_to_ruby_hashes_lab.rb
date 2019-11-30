3fc2aac7094a0ef78e2c3752fe745fe733085a25
Catherine Smiley <catherinesmiley@gmail.com>
2019-11-30 22:09:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
   {
 	  :railroads => {
 	    :pieces => 4, :rent_in_dollars => {
-	      :one_piece_owned => 25, "2" => 5.05, "3" => 5.06, "4"=> 5.07},
+	      :one_piece_owned => 25, :two_pieces_owned => 50, "3" => 5.06, "4"=> 5.07},
 	    :names => {
 	      "a" => "Adam", "b" => "Bob", "c" => "Colin", "d" => "Dan"}
 	    }
